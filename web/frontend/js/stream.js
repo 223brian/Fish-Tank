a3d621ebbac94a72aea5cdc3655d6c08463ab8c7
Noah Adams <noahgadams07@gmail.com>
2026-03-08 23:39:43 -0400

    hopefully correctly added camera stream? (I'll need to come back and make it more modular if it works)

--- web/frontend/js/stream.js
+++ web/frontend/js/stream.js
@@ -1,6 +1,5 @@
-const stream1 = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
-const stream2 =
-    "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8";
+const stream1 = "http://100.82.82.83:8888/cam1/stream.m3u8";
+const stream2 = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
 
 let currentCamera = 1;
 let hls = null;
